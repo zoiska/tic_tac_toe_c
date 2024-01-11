6fbc395b1fe1e4f0c38f246954e050035210b57f
zoisk <zoisk64@gmail.com>
2024-01-11 01:27:50 +0100

    u

--- logic.c
+++ logic.c
@@ -1,18 +1,34 @@
 #include "logic.h"
 
+#define MAX_RAND 8
+
 void gamemode(char *box){
     int gm;
-    printf("Eingabe <1> fuer Einzelspieler, Eingabe <2> fuer zwei Spieler.\n");
+    printf("Eingabe <1> fuer zwei Spieler, Eingabe <2> fuer Einzelspieler.\n");
     scanf("%d", &gm);
     if(gm == 1){
-        //ein_Spieler();
+        zwei_Spieler(box);
     }
     else if(gm == 2){
-        zwei_Spieler(box);
+        ein_Spieler(box);
     }
     else {
         printf("Ungueltige Eingabe.\n");
     }
+}
+
+void ein_Spieler(char *box){
+    print_Board();
+    eing_x(box);
+    o_Random(box);
+    eing_x(box);
+    o_Random(box);
+    eing_x(box);
+    o_Random(box);
+    eing_x(box);
+    o_Random(box);
+    eing_x(box);
+    printf("Unentschieden!");
 }
 
 void zwei_Spieler(char *box){
@@ -26,6 +42,7 @@ void zwei_Spieler(char *box){
     eing_x(box);
     eing_o(box);
     eing_x(box);
+    printf("Unentschieden!\n\n");
 }
 
 void eing_x(char *box){
@@ -58,6 +75,25 @@ void eing_o(char *box){
     }
 }
 
+void o_Random(char *box){
+    //srand(time(NULL));
+    //int r = rand() % MAX_RAND;
+    //if(box[r] != '_'){
+        for(int i = 0; i <= 8; i++){
+            if(box[i] == '_'){
+                box[i] = 'O';print_Current(box);
+                check_Win(box, 'O');
+                break;
+            }
+        }
+    //}
+    //else {
+    //    box[r] = 'O';
+    //    print_Current(box);
+    //    check_Win(box, 'O');
+    //}
+}
+
 void check_Win(char *box, char player){
     if(box[0] == player && box[1] == player && box[2] == player
     || box[3] == player && box[4] == player && box[5] == player
@@ -65,9 +101,9 @@ void check_Win(char *box, char player){
     || box[0] == player && box[3] == player && box[6] == player
     || box[1] == player && box[4] == player && box[7] == player
     || box[2] == player && box[5] == player && box[8] == player
-    || box[0] == player && box[5] == player && box[8] == player
-    || box[2] == player && box[5] == player && box[6] == player){
-        printf("Spieler <%c> hat gewonnen!", player);
+    || box[0] == player && box[4] == player && box[8] == player
+    || box[2] == player && box[4] == player && box[6] == player){
+        printf("Spieler <%c> hat gewonnen!\n\n", player);
         exit(0);
     }
 }
--- logic.h
+++ logic.h
@@ -1,14 +1,18 @@
 #include "board.h"
 #include <stdlib.h>
+#include <time.h>
 
 #ifndef LOGIC_H
 #define LOGIC_H
 
 void gamemode(char *box);
 void zwei_Spieler(char *box);
+void ein_Spieler(char *box);
 void ein_Spieler();
 void eing_x(char *box);
 void eing_o(char *box);
+void o_Random(char *box);
 void check_Win(char *box, char player);
+void unentschieden();
 
 #endif //LOGIC_H
